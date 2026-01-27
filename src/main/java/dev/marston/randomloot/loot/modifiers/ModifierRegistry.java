84e0d77fddb4187e717242a5a4bfc6c85e7324bd
marston <marston@jackallabs.io>
2026-01-27 12:19:37 -0500

    Add Feasting modifier - hunger-based performance

--- src/main/java/dev/marston/randomloot/loot/modifiers/ModifierRegistry.java
+++ src/main/java/dev/marston/randomloot/loot/modifiers/ModifierRegistry.java
@@ -82,13 +82,14 @@ public class ModifierRegistry {
 	public static Modifier BUSTED = register(new Busted());
 
 	public static Modifier UNBREAKING = register(new Unbreaking());
+	public static Modifier FEASTING = register(new Feasting());
 
 	public static final Set<Modifier> BREAKERS = Set.of(EXPLODE, LEARNING, ATTRACTING, VEINY, MELTING, EXCAVATOR, PROSPECTOR);
 	public static final Set<Modifier> USERS = Set.of(TORCH_PLACE, DIRT_PLACE, FIRE_PLACE, FIRE_BALL, VOID_TOUCHED);
 	public static final Set<Modifier> HURTERS = Set.of(CRITICAL, CHARGING, FLAMING, COMBO, DRAINING, POISONOUS,
-			WITHERING, BLINDING, BEZERK, NEMESIS, SOULBOUND, SCORCHED, FROZEN, OVERGROWN);
+			WITHERING, BLINDING, BEZERK, NEMESIS, SOULBOUND, SCORCHED, FROZEN, OVERGROWN, FEASTING);
 	public static final Set<Modifier> HOLDERS = Set.of(HASTY, ABSORBTION, FILLING, RAINY, ORE_FINDER, SPAWNER_FINDER,
-			LIVING, REGENERATING, RESISTANT, FIRE_RESISTANT, AQUATIC);
+			LIVING, REGENERATING, RESISTANT, FIRE_RESISTANT, AQUATIC, FEASTING);
 
 	public static final Set<Modifier> STATS = Set.of(BUSTED);
 
