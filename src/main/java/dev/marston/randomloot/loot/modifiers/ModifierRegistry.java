d124eede6bedc233494566fbd4f71ce01c8822b9
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2026-01-27 14:50:55 -0500

    Add Crowd Pleaser modifier (#24)
    * Add Crowd Pleaser modifier - bonus damage with nearby same-type mobs
    
    * Fix damage immunity preventing bonus damage

--- src/main/java/dev/marston/randomloot/loot/modifiers/ModifierRegistry.java
+++ src/main/java/dev/marston/randomloot/loot/modifiers/ModifierRegistry.java
@@ -60,6 +60,7 @@ public class ModifierRegistry {
 	public static Modifier NEMESIS = register(new Nemesis());
 	public static Modifier SOULBOUND = register(new Soulbound());
 	public static Modifier EXECUTIONER = register(new Executioner());
+	public static Modifier CROWD_PLEASER = register(new CrowdPleaser());
 
 	// Biome-restricted modifiers
 	public static Modifier AQUATIC = register(new Aquatic());
@@ -90,7 +91,7 @@ public class ModifierRegistry {
 	public static final Set<Modifier> BREAKERS = Set.of(EXPLODE, LEARNING, ATTRACTING, VEINY, MELTING, EXCAVATOR, PROSPECTOR);
 	public static final Set<Modifier> USERS = Set.of(TORCH_PLACE, DIRT_PLACE, FIRE_PLACE, FIRE_BALL, VOID_TOUCHED);
 	public static final Set<Modifier> HURTERS = Set.of(CRITICAL, CHARGING, FLAMING, COMBO, DRAINING, POISONOUS,
-			WITHERING, BLINDING, BEZERK, NEMESIS, SOULBOUND, SCORCHED, FROZEN, OVERGROWN, FIERCE, FEASTING, EXECUTIONER);
+			WITHERING, BLINDING, BEZERK, NEMESIS, SOULBOUND, SCORCHED, FROZEN, OVERGROWN, FIERCE, FEASTING, EXECUTIONER, CROWD_PLEASER);
 	public static final Set<Modifier> HOLDERS = Set.of(HASTY, ABSORBTION, FILLING, RAINY, ORE_FINDER, SPAWNER_FINDER,
 			LIVING, REGENERATING, RESISTANT, FIRE_RESISTANT, AQUATIC, HUNTER, FEASTING);
 
